975fbbb71da39d7d9c5a124ada5703dfbba2e0ba
Diego Contreras <166648552+DieEmaContreras@users.noreply.github.com>
2024-06-16 19:48:30 -0300

    Update script

--- scrip.js
+++ scrip.js
@@ -1,1 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    const usernameInput = document.getElementById ('username');    
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const username = usernameInput.value.trim(); 
+
+        if (username) {
+            localStorage.setItem('username', username); 
+            alert('Nombre de usuario almacenado en localStorage.');
+           
+        } else {
+            document.getElementById('error-message').textContent = 'Por favor, ingresa tu nombre de usuario.';
+        }
+    });
+});
 
