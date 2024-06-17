8d1a49d1f72487f0c831c4a680111f35d7c3f6c2
Eros Panuccio <erospanuccio@gmail.com>
2024-06-17 19:39:50 -0300

    Login + Profile changes

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('form');
-    const usernameInput = document.getElementById ('username');    
+const LOGIN_FORM = document.querySelector('form');
+const USERNAME_INPUT = document.getElementById ('username');
+const PASSWORD_INPUT = document.getElementById ('password');  
+const LOGIN_ERROR_MSG = document.querySelector("#login-error-message")
 
-    form.addEventListener('submit', function(event) {
+String.prototype.hashCode = function() {
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        chr = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+function isLoginInfoOk(){
+    if (USERNAME_INPUT.value != localStorage.getItem("username"))
+        return "El usuario ingresado no es correcto."
+    
+    if (PASSWORD_INPUT.value.hashCode() != localStorage.getItem("password"))
+        return "La contraseña ingresada no es correcta."
+
+    return "OK"
+}
+
+LOGIN_FORM.addEventListener('submit', function(event) {
+    const username = USERNAME_INPUT.value.trim();
+    const password = PASSWORD_INPUT.value.trim();
+    let error_msg = isLoginInfoOk()
+
+    if ( error_msg === 'OK' ) {
+        LOGIN_ERROR_MSG.style.visibility = 'hidden';
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password.hashCode());
+    } else {
         event.preventDefault();
-
-        const username = usernameInput.value.trim(); 
-
-        if (username) {
-            localStorage.setItem('username', username); 
-            alert('Nombre de usuario almacenado en localStorage.');
-           
-        } else {
-            document.getElementById('error-message').textContent = 'Por favor, ingresa tu nombre de usuario.';
-        }
-    });
-});
-
+        LOGIN_ERROR_MSG.textContent = error_msg;
+        return false
+    }
+}, false);
--- scripts/profile.js
+++ scripts/profile.js
@@ -8,10 +8,9 @@ const CARD_VENC = document.querySelector(".credit-card .credit-card-col2 #vencim
 const CARD_NUMBER = document.querySelector(".credit-card .input_style_1[type='number']")
 const CARD_NAME_LABEL = document.querySelector(".credit-card .credit-card-col1 .name-label")
 const CARD_NAME = document.querySelector(".credit-card .input_style_1[type='text']")
-
-const SHOW_PASSWORD_DELAY_TIME = 1500
-const DELAY = ms => new Promise(res => setTimeout(res, ms));
-const SHOW_MESSAGES = document.querySelectorAll(".show-password")
+const USERNAME_H1 = document.querySelector(".user-name h1#username")
+const PASSWORD_ERROR_MSG = document.querySelector("#password-error-message")
+const PAY_METHOD_ERROR_MSG = document.querySelector("#pay-method-error-message")
 
 const TRANSFER_INPUT = document.querySelector(".transfer input")
 const TRANSFER_P = document.querySelector(".transfer p")
@@ -21,11 +20,19 @@ const USER_EMAIL = document.querySelector(".change-password input[id='email']")
 const USER_ACTUAL_PASSWORD = document.querySelector(".change-password input[id='password']")
 const USER_NEW_PASSWORD = document.querySelector(".change-password input[id='new-password']")
 const USER_REPEAT_NEW_PASSWORD = document.querySelector(".change-password input[id='repeat-password']")
+
+const SHOW_PASSWORD_DELAY_TIME = 1500
+const DELAY = ms => new Promise(res => setTimeout(res, ms));
+const SHOW_MESSAGES = document.querySelectorAll(".show-password")
+
 const REGEX_LETTERS_AND_SPACES = /^[a-zA-Z\s]*$/;
 const REGEX_NUMBERS = /^[0-9]*$/;
-const REGEX_ALPHANUM = /^\w+$/;
 const REGEX_EMAIL = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const REGEX_YEAR_MONTH = /([12]\d{3}-(0[1-9]|1[0-2]))/
+const REGEX_ATLEAST2_SPECIAL = /(?:[^`!@#$%^&*\-_=+'\/.,]*[`!@#$%^&*\-_=+'\/.,]){2}/
+const REGEX_ATLEAST2_LETTERS = /(?:[^a-zA-Z]*[a-zA-Z]){2}/
+const REGEX_ATLEAST2_NUMBERS = /(?:[^\d]*[\d]){2}/
+const REGEX_3_NUMBERS_NOT_ZERO = /^[1-9]{3}$/
 const URL_VISTA = document.location.href
 const URL_GEN = new URL(URL_VISTA)
 
@@ -58,39 +65,93 @@ function fixCardNumberAndCvvLength() {
         CARD_NAME.value = CARD_NAME.value.substring(0, 50)
 }
 
+function sumNumbersInString(str) {
+    let sum = 0;
+
+    for (char in str) {
+        if ( Number.isInteger( Number(str[char])) )
+            sum += Number(str[char])
+    }
+
+    return sum;
+}
+
 function isCardInfoOk() {
     let date_venc = new Date(CARD_VENC.value)
     month_venc = date_venc.getUTCMonth() + 1
     year_venc = date_venc.getUTCFullYear()
-
-    // Refactorizar cuando haga los error-messages
-    if (    CARD_NUMBER.value.length === 16 &&
-            CVV.value.length === 3 &&
-            !isNaN(Number(CARD_NUMBER.value)) &&
-            !isNaN(Number(CVV.value)) &&
-            CARD_NAME.value.length <= 50 &&
-            CARD_NAME.value.length > 0 &&
-            REGEX_LETTERS_AND_SPACES.test(CARD_NAME.value) &&
-            REGEX_YEAR_MONTH.test(CARD_VENC.value) &&
-            ( ( month_venc >= TODAY_MONTH && year_venc == TODAY_YEAR ) || year_venc > TODAY_YEAR )
-        )
-        return true
+    let sumCardNumbers;
     
-    return false
+    if ( !isNaN(Number(CARD_NUMBER.value)) )
+        sumCardNumbers = sumNumbersInString( String(CARD_NUMBER.value.substring(0, CARD_NUMBER.value.length - 1)) )
+
+    if ( sumCardNumbers % 2 == 0 && CARD_NUMBER.value.substring(CARD_NUMBER.value.length - 1, CARD_NUMBER.value.length) % 2 == 0 )
+        return "Si la suma de los números de la tarjeta (sin incluir el último) es par, el último número debe ser par"
+    
+    if ( sumCardNumbers % 2 == 1 && CARD_NUMBER.value.substring(CARD_NUMBER.value.length - 1, CARD_NUMBER.value.length) % 2 == 1 )
+        return "Si la suma de los números de la tarjeta (sin incluir el último) es impar, el último número debe ser par"
+
+    if ( CARD_NUMBER.value.length < 16 || CARD_NUMBER.value.length > 19 )
+        return "El número de la tarjeta debe tener entre 16 y 19 caracteres de longitud."
+
+    if ( CVV.value.length != 3 )
+        return "El número de CVV/CVC debe tener 3 caracteres de longitud."
+
+    if ( !REGEX_3_NUMBERS_NOT_ZERO.test(CVV.value) )
+        return "El número de CVV/CVC debe ser de 3 números distintos de 0 (cero)."
+
+    if ( isNaN(Number(CARD_NUMBER.value)) )
+        return "El número de la tarjeta sólo debe contener números."
+
+    if ( isNaN(Number(CVV.value)) )
+        return "El número de CVV/CVC sólo debe contener números."
+
+    if ( !REGEX_LETTERS_AND_SPACES.test(CARD_NAME.value) )
+        return "El nombre del titular debe contener sólo letras y espacios."
+
+    if ( !REGEX_YEAR_MONTH.test(CARD_VENC.value) )
+        return "El mes de vencimiento debe tener el formato correcto."
+
+    if ( !(month_venc >= TODAY_MONTH && year_venc == TODAY_YEAR ) || !(year_venc > TODAY_YEAR) )
+        return "El mes de vencimiento debe ser mayor o igual al mes actual."
+
+    return "OK"
+}
+
+String.prototype.hashCode = function() {
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+        chr = this.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
 }
 
 function isChangePasswordInfoOk() {
-    if ( 
-        REGEX_EMAIL.test(USER_EMAIL.value) && 
-        REGEX_ALPHANUM.test(USER_ACTUAL_PASSWORD.value) && 
-        REGEX_ALPHANUM.test(USER_NEW_PASSWORD.value) && 
-        REGEX_ALPHANUM.test(USER_REPEAT_NEW_PASSWORD.value) && 
-        USER_ACTUAL_PASSWORD.value != USER_NEW_PASSWORD.value &&
-        USER_NEW_PASSWORD.value === USER_REPEAT_NEW_PASSWORD.value
-    )
-        return true
-    
-    return false
+    if ( !REGEX_EMAIL.test(USER_EMAIL.value) )
+        return "Formato inválido de email."
+
+    if ( !USER_ACTUAL_PASSWORD.value )
+        return "Debe ingresar la contraseña actual."
+
+    if ( USER_ACTUAL_PASSWORD.value.hashCode() != localStorage.getItem("password") )
+        return "La contraseña actual es incorrecta."
+
+    if ( USER_NEW_PASSWORD.value.length < 8 )
+        return "La nueva contraseña debe tener al menos 8 caracteres de longitud."
+
+    if ( !REGEX_ATLEAST2_LETTERS.test(USER_NEW_PASSWORD.value) || !REGEX_ATLEAST2_SPECIAL.test(USER_NEW_PASSWORD.value) || !REGEX_ATLEAST2_NUMBERS.test(USER_NEW_PASSWORD.value) )
+        return "La nueva contraseña debe contener al menos 2 letras, 2 números y 2 caracteres especiales."
+
+    if ( USER_NEW_PASSWORD.value != USER_REPEAT_NEW_PASSWORD.value )
+        return "La nueva contraseña debe escribirse igual ambas veces."
+
+    if ( USER_ACTUAL_PASSWORD.value == USER_NEW_PASSWORD.value )
+        return "La nueva contraseña debe ser diferente a la actual."
+
+    return "OK"
 }
 
 function showCardInfo() {
@@ -142,6 +203,7 @@ function showChangePasswordModal() {
 }
 
 function setDefaultProfileValues() {
+    USERNAME_H1.textContent = localStorage.getItem("username")
     changeVisibility(TRANSFER_P, HIDDEN_ELEMENT)
     CARD_NUMBER.required = true
     CARD_NAME.required = true
@@ -176,10 +238,15 @@ function setDefaultListeners() {
             if (INPUT_CREDIT.checked === true) {
                 fixCardNumberAndCvvLength()
                 showCardInfo()
-                if ( isCardInfoOk() )
+                pay_method_error_msg = isCardInfoOk()
+                if ( pay_method_error_msg == "OK" ) {
+                    changeVisibility(PAY_METHOD_ERROR_MSG, HIDDEN_ELEMENT)
                     changeBtnState(PAY_METHOD_BTN, ENABLE_STATE)
-                else
+                } else {
+                    PAY_METHOD_ERROR_MSG.textContent = pay_method_error_msg
+                    changeVisibility(PAY_METHOD_ERROR_MSG, VISIBLE_ELEMENT)
                     changeBtnState(PAY_METHOD_BTN, DISABLE_STATE)
+                }
             } else {
                 hideCardInfo()
                 changeBtnState(PAY_METHOD_BTN, ENABLE_STATE)
@@ -194,10 +261,15 @@ function setDefaultListeners() {
     
     CHANGE_PASSWORD_INPUTS.forEach(input => {
         input.addEventListener("input", function() {
-            if ( isChangePasswordInfoOk() )
+            let password_error_msg = isChangePasswordInfoOk()
+            if ( password_error_msg === "OK" ) {
+                changeVisibility(PASSWORD_ERROR_MSG, HIDDEN_ELEMENT)
                 changeBtnState(CHANGE_PASSWORD_BTN, ENABLE_STATE)
-            else
-            changeBtnState(CHANGE_PASSWORD_BTN, DISABLE_STATE)
+            } else {
+                PASSWORD_ERROR_MSG.textContent = password_error_msg
+                changeVisibility(PASSWORD_ERROR_MSG, VISIBLE_ELEMENT)
+                changeBtnState(CHANGE_PASSWORD_BTN, DISABLE_STATE)
+            }
         })
     });
     
