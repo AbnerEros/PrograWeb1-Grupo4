cc6e819ebf5f32425de57bc2d9940ed95dd4f86f
SantiagoHavelka <165961278+SantiagoHavelka@users.noreply.github.com>
2024-06-18 18:19:17 -0300

    Cambios en home-movies y home-series

---
+++ scripts/home-movies.js
@@ -0,0 +1,20 @@
+const CONTENT_SERIES = JSON.parse(localStorage.getItem("series_list"));
+const CONTENT_MOVIES = JSON.parse(localStorage.getItem("movies_list"));
+const PREFIX_PATH_IMAGES_MOVIES = "../img/movies/";
+const PREFIX_PATH_IMAGES_SERIES = "../img/series/";
+
+function addImages(){
+for(let i in CONTENT_MOVIES){
+    const MOVIES_AND_SERIES_SECTION = document.querySelector(".grid-container");
+    const a = document.createElement("a");
+    a.href =  CONTENT_MOVIES[i]["html_link"] + CONTENT_MOVIES[i]["id"];
+    const img = document.createElement("img");
+    img.classList.add("movie-poster");
+    img.src = PREFIX_PATH_IMAGES_MOVIES + CONTENT_MOVIES[i]["img_src"];
+    img.alt = CONTENT_MOVIES[i]["serie_name"];
+    a.appendChild(img);
+    MOVIES_AND_SERIES_SECTION.appendChild(a);
+    console.log(a.href);
+}
+}
+addImages();
