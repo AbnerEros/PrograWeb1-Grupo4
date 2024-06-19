d6e88f01417b08c6781bc67fce6524900d589d28
SantiagoHavelka <165961278+SantiagoHavelka@users.noreply.github.com>
2024-06-19 17:17:46 -0300

    Filtro categorias en Home series y Home movies

--- scripts/home-movies.js
+++ scripts/home-movies.js
@@ -5,7 +5,7 @@ const PREFIX_PATH_IMAGES_SERIES = "../img/series/";
 const SELECT_CATEGORY = document.querySelector(".categories select.category-select")
 const MOVIES_AND_SERIES_SECTION = document.querySelector(".grid-container");
 
-function addImages(){
+function addMovies(){
     for(let i in CONTENT_MOVIES){
         const a = document.createElement("a");
         a.href =  CONTENT_MOVIES[i]["html_link"] + CONTENT_MOVIES[i]["id"];
@@ -19,7 +19,28 @@ function addImages(){
     }
 }
 
-addImages();
+function updateMoviesAndSeries(category) {
+    const MOVIES_AND_SERIES_SECTION_CHILDS = document.querySelectorAll('.grid-container a');
+    MOVIES_AND_SERIES_SECTION_CHILDS.forEach(element => {
+        element.remove()
+    })
+    
+    for(let i in CONTENT_MOVIES) {
+        if ( !CONTENT_MOVIES[i].genre.normalize('NFD').replace(/[\u0300-\u036f]/g,"").toLowerCase().includes(category) )
+            continue;
+
+        const a = document.createElement("a");
+        const img = document.createElement("img");
+        a.href =  CONTENT_MOVIES[i]["html_link"] + CONTENT_MOVIES[i]["id"];
+        img.classList.add("movie-poster");
+        img.src = PREFIX_PATH_IMAGES_MOVIES + CONTENT_MOVIES[i]["img_src"];
+        img.alt = CONTENT_MOVIES[i]["serie_name"];
+        a.appendChild(img);
+        MOVIES_AND_SERIES_SECTION.appendChild(a);
+    }
+}
+
+addMovies();
 
 SELECT_CATEGORY.addEventListener("change", function() {
     updateMoviesAndSeries(SELECT_CATEGORY.value)
--- scripts/home-series.js
+++ scripts/home-series.js
@@ -3,10 +3,12 @@ const CONTENT_MOVIES = JSON.parse(localStorage.getItem("movies_list"));
 const PREFIX_PATH_IMAGES_MOVIES = "../img/movies/";
 const PREFIX_PATH_IMAGES_SERIES = "../img/series/";
 
+const SELECT_CATEGORY = document.querySelector(".categories select.category-select")
+const MOVIES_AND_SERIES_SECTION = document.querySelector(".grid-container");
 
-function addImages(){
+
+function addSeries(){
     for(let i in CONTENT_SERIES){
-        const MOVIES_AND_SERIES_SECTION = document.querySelector(".grid-container");
         const a = document.createElement("a");
         a.href =CONTENT_SERIES[i]["html_link"] + CONTENT_SERIES[i]["id"];
         const img = document.createElement("img");
@@ -17,4 +19,27 @@ function addImages(){
         MOVIES_AND_SERIES_SECTION.appendChild(a);
     }
     }
-addImages();
+    
+function updateMoviesAndSeries(category) {
+    const MOVIES_AND_SERIES_SECTION_CHILDS = document.querySelectorAll('.grid-container a');
+    MOVIES_AND_SERIES_SECTION_CHILDS.forEach(element => {
+        element.remove()
+    })
+    for(let i in CONTENT_SERIES) {
+        if ( !CONTENT_SERIES[i].genre.normalize('NFD').replace(/[\u0300-\u036f]/g,"").toLowerCase().includes(category) )
+            continue;
+
+        const a = document.createElement("a");
+        const img = document.createElement("img");
+        a.href =CONTENT_SERIES[i]["html_link"] + CONTENT_SERIES[i]["id"];
+        img.classList.add("movie-poster");
+        img.src = PREFIX_PATH_IMAGES_SERIES + CONTENT_SERIES[i]["img_src"];
+        img.alt = CONTENT_SERIES[i]["serie_name"];
+        a.appendChild(img);
+        MOVIES_AND_SERIES_SECTION.appendChild(a);
+    }
+}
+addSeries();
+SELECT_CATEGORY.addEventListener("change", function() {
+    updateMoviesAndSeries(SELECT_CATEGORY.value)
+})
