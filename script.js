95b0d8da8bf40792a0ec10885506baaebc22cf04
Eros Panuccio <erospanuccio@gmail.com>
2024-07-01 19:55:24 -0300

    Fix close session mobile

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const HEADER_CLOSE_SESSION = document.getElementById("header-close-session")
+const HEADER_CLOSE_SESSION = document.querySelectorAll("#header-close-session")
 
-if ( HEADER_CLOSE_SESSION ) {
-    HEADER_CLOSE_SESSION.addEventListener("click", function() {
-        localStorage.removeItem("username");
-        localStorage.removeItem("email");
-        localStorage.removeItem("password");
-        localStorage.removeItem("pay_method");
-        localStorage.removeItem("pay_method_card");
-        localStorage.removeItem("pay_method_card_name");
-        localStorage.removeItem("pay_method_card_venc");
-        localStorage.removeItem("pay_method_cvv");
-        window.location.replace("../index.html")
-    }, false)
-}
+HEADER_CLOSE_SESSION.forEach(btn => {
+    if ( btn ) {
+        btn.addEventListener("click", function() {
+            localStorage.removeItem("username");
+            localStorage.removeItem("email");
+            localStorage.removeItem("password");
+            localStorage.removeItem("pay_method");
+            localStorage.removeItem("pay_method_card");
+            localStorage.removeItem("pay_method_card_name");
+            localStorage.removeItem("pay_method_card_venc");
+            localStorage.removeItem("pay_method_cvv");
+            window.location.replace("../index.html")
+        }, false)
+    } 
+});
